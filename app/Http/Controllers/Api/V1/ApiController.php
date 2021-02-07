04ace3babc840535a8515009a3727bcc59448935
martyeugen <martyneugene97@gmail.com>
2021-02-07 21:28:27 +0300

    Bad request error

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -13,13 +13,23 @@ class ApiController extends Controller
 
     private $bitcoin_rates;
 
-    public function index()
+    public function index(Request $request)
     {
+        $method = $request->input('method');
+        if(!method_exists($this, $method))
+        {
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Bad request'
+            ];
+
+            return response()->json($data, 400);
+        }
+
         $response = Http::get(self::BITCOIN_TICKER_URL);
         if(!$response->ok())
-        {
             abort(503);
-        }
 
         $this->bitcoin_rates = $response->json();
         if(is_array($this->bitcoin_rates)) {
--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -41,4 +41,21 @@ class ApiTest extends TestCase
                 'code' => 200
             ]);
     }
+
+    /**
+     * If method doesn't exist, respond with 400 bad request error
+     * @test
+     * @return void
+     */
+    public function bad_request()
+    {
+        $url = '/api/v1';
+        $response = $this->withHeader('Authorization', 'Bearer ' . env('API_TOKEN'))->get($url);
+        $response->assertStatus(400)
+            ->assertExactJson([
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Bad request'
+            ]);
+    }
 }
